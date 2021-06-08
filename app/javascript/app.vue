2608e6d4af99941ed461f14f9be038679dc488eb
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-06-08 23:02:31 +0200

    Add and setup axios plug front with api

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,14 +1,32 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+    <ul class="mt-4">
+      <li class="py-4" v-for="feedback in feedbacks" :key="feedback.id" :feedback="feedback">
+        <p>{{ feedback.title }}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { api } from './packs/api/client';
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      message: "Hello! Here you will find the feedbacks",
+      feedbacks: []
+    }
+  },
+  mounted() {
+    this.loadFeedback();
+  },
+  methods: {
+    loadFeedback() {
+      return api.feedbacks().then((response) => {
+        console.log('coucou')
+        this.feedbacks = response.data;
+      });
     }
   }
 }
