66dba01a746860d4f204b4a8395956035033731a
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-06-09 14:55:26 +0200

    Pass props between component

--- app/javascript/components/FeedbackCard.vue
+++ app/javascript/components/FeedbackCard.vue
@@ -4,50 +4,56 @@
       <h3>{{ title }}</h3>
       <p>{{ description }}</p>
     </div>
-    <div>{{ done ? "✅" : "⭕️"  }}</div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      title: "Share your feedback",
-      description: "Give us some more info",
-      done: false
-    }
-  }
+  props: {
+    title: String,
+    description: String,
+    done: Boolean,
+  },
 };
 </script>
 
-<style scoped lang="scss">
-  .task-card {
+<style lang="scss">
+.task-card {
+  display: flex;
+  text-align: left;
+  background-color: saturate(rgba(#41b883, 0.03), 30%);
+  transition: all 0.3s;
+  min-height: 6rem;
+  border-bottom: solid 1px rgba(#35495e, 0.1);
+  border-left: solid 8px #41b883;
+  p {
+    font-size: 0.9rem;
+  }
+  &:hover {
+    background-color: white;
+    transform: scale(1.02);
+    box-shadow: 2px 3px 10px rgba(black, 0.1);
+  }
+  & > div:first-child {
+    margin: 0 1rem;
+    padding: 1rem 0;
+    flex-grow: 1;
     display: flex;
-    min-height: 6rem;
-    text-align: left;
-    background-color: saturate(rgba(#41b883, 0.03), 30%);
-    border-bottom: solid 1px rgba(#35495e, 0.1);
-    border-left: solid 8px #41b883;
-    transition: all 0.3s;
-    p { font-size: 0.9rem; }
+    flex-direction: column;
+    justify-content: center;
+  }
+  &.done {
+    border-left: solid 8px rgba(#35495e, 0.3);
+    background-color: rgba(#35495e, 0.08);
+    h3,
+    p {
+      text-decoration: line-through;
+      opacity: 0.3;
+    }
     &:hover {
-      transform: scale(1.02);
-      background-color: white;
-      box-shadow: 2px 3px 10px rgba(black, 0.1);
-    }
-    & > div:first-child {
-      margin: 0 1rem;
-      padding: 1rem 0;
-      display: flex;
-      flex-grow: 1;
-      flex-direction: column;
-      justify-content: center;
-    }
-    &.done {
-      border-left: solid 8px rgba(#35495e, 0.3);
-      background-color: rgba(#35495e, 0.08);
-      h3, p { text-decoration: line-through; opacity: 0.3; }
-      &:hover { transform: unset; box-shadow: unset; }
+      transform: unset;
+      box-shadow: unset;
     }
   }
+}
 </style>
--- app/javascript/components/FeedbacksList.vue
+++ app/javascript/components/FeedbacksList.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <FeedbackCard />
+    <FeedbackCard 
+      v-for="(feedback, index) in feedbacks"
+      :key="index"
+      :title="feedback.title"
+      :description="feedback.description"
+      :done="feedback.done"
+    />
   </div>
 </template>
 
@@ -10,6 +16,30 @@ import FeedbackCard from "./FeedbackCard";
 export default{
   components: {
     FeedbackCard
-  }
+  },
+  data() {
+    return {
+      feedbacks: [
+        {
+          title: "Create a card component",
+          description:
+            "Create a new TaskCard.vue file in the components folder, then import it in TasksList",
+          done: true,
+        },
+        {
+          title: "Make the card component dynamic",
+          description:
+            "Learn about using the data option and passing data to child components using props",
+          done: true,
+        },
+        {
+          title: "Bind the attributes to the data",
+          description:
+            "Use the v-bind directive to bind the title and description to our data",
+          done: false,
+        },
+      ],
+    };
+  },
 }
 </script>
