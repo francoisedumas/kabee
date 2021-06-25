e5d9fdfc760148abb28396d66154f3d99003005e
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-06-25 16:55:50 +0200

    Add rating with stars

--- app/javascript/components/FeedbackCard.vue
+++ app/javascript/components/FeedbackCard.vue
@@ -3,16 +3,26 @@
     <div>
       <h3>{{ title }}</h3>
       <p>{{ description }}</p>
+      <Rating
+        :grade="3"
+        :maxStars="5"
+        :hasCounter="true"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import Rating from './Rating.vue'
+
 export default {
   props: {
     title: String,
     description: String,
     rating: Number,
+  },
+  components: {
+    Rating
   },
 };
 </script>
